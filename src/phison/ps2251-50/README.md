c9e020ae261112dde630ad1fd82e01ef285da463
Sheep Sun <sunxiaoyang2003@gmail.com>
2023-04-01 20:21:20 +0800

    Add PS2251-50

---
+++ src/phison/ps2251-50/README.md
@@ -0,0 +1,34 @@
+# PS2251-50
+
+## Info
+
+- CPU
+  - MCS-51 (8051) compatible
+
+## Usage
+
+
+### Dependencies
+```
+sudo apt install sdcc sg3-utils
+```
+
+### Build
+```
+make
+```
+
+### Load
+Assuming the drive is `/dev/sda`
+```
+sudo sg_raw -s 512 /dev/sda 06 b1 03 00 00 00 00 00 01 00 00 00 00 00 00 00 < header.bin
+sudo sg_raw -s 4096 /dev/sda 06 b1 02 00 00 00 00 00 08 00 00 00 00 00 00 00 < firmware.bin
+```
+
+### Run
+```
+sudo sg_raw /dev/sda 06 b3 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+```
+Now the flash drive's status LED will start blinking at fixed period. It will stop responding to USB. Replug the drive to get back to normal state.
+
+
---
+++ src/phison/ps2251-50/main.c
@@ -0,0 +1,43 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <mcs51/8051.h>
+
+#define	MSB(word)      (BYTE)(((WORD)(word) >> 8) & 0xff)
+#define LSB(word)      (BYTE)((WORD)(word) & 0xff)
+
+#define XVAL(addr)     (*( __xdata volatile unsigned char  *)(addr))
+#define IVAL(addr)     (*( __idata volatile unsigned char  *)(addr))
+
+void usb_isr (void) __interrupt {
+	EX0 = 0;
+}
+
+void ep_isr (void) __interrupt {
+	EX1 = 0;
+}
+
+void timer0_isr (void) __interrupt {
+	ET0 = 0;
+}
+
+void timer1_isr (void) __interrupt {
+	ET1 = 0;
+}
+
+
+int main(void) {
+   	int i, j;
+	while(1){
+		__asm__("clr p1.1");
+		for(i = 0; i <= 10000; i++)
+			for(j = 0; j <= 100; j++);
+		__asm__("setb p1.1");
+		for(i = 0; i <= 10000; i++)
+			for(j = 0; j <= 100; j++);
+	}
+	return 0;
+}
+
